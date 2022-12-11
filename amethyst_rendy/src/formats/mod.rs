db1f1196c6147f86e5f902d3d6b0bd061c4fa3dd
lullaby66 <94006475+lullaby66@users.noreply.github.com>
2022-12-11 13:08:43 +0100

    Delete amethyst_rendy directory

--- amethyst_rendy/src/formats/mod.rs
+++
@@ -1,3 +0,0 @@
-//! Pre-defined graphical formats and data provided by `amethyst_rendy`
-pub mod mesh;
-pub mod texture;
--- amethyst_rendy/src/transparent.rs
+++
@@ -1,5 +0,0 @@
-//! Transparency component implementation
-
-/// Transparent mesh component
-#[derive(Clone, Copy, Debug, Default, serde::Serialize, serde::Deserialize)]
-pub struct Transparent;
